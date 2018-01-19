fbfe37415a979bc557eccabf66d1cbc3e0bd99f5
antoine.caccin@etud.u-picardie.fr <antoine.caccin@etud.u-picardie.fr>
2018-01-19 01:54:38 +0100

    no message

--- Showtimes.php
+++ Showtimes.php
@@ -1,7 +1,9 @@
 <?php
 header('Content-Type: application/json');
-$showtimes = array("Id" => array(1, 2, 3),"name" => array("jhgjhg","hjgkg","chhs"));
 
-    foreach ($showtimes as $key => $value)
-    echo (json_encode("$key : $value"));
+  $showtimes = array("id" => array(1, 2, 3),"name" => array("jhgjhg","hjgkg","chhs"));
+  foreach ($variable as $key => $value {
+    echo (json_encode("Id : $key; name : $value"));
+
+
 ?>
