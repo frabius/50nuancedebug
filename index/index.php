3ed3cf91c6903ec02e4ee7b95bbc43eb8f976528
DESKTOP-41PK2FD\keke8 <kerian.monierpechet@gmail.com>
2018-01-19 01:10:56 +0100

    modification index affichage requete

--- index/index.php
+++ index/index.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents("80.211.145.93/test.php");
+$json = file_get_contents("http://80.211.145.93/theater/");
 $data = json_decode($json);
 ?>
 
@@ -11,6 +11,7 @@ $data = json_decode($json);
     <title>50 nuances de film</title>
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <link rel="stylesheet" type="text/css" href="simplegrid.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset='utf-8'/>
     <link rel="shortcut icon" href="imgonglet.png">
@@ -25,7 +26,7 @@ $data = json_decode($json);
     <div class="grid grid-pad" style="padding: 0;">
         <div class="col-1-6">
             <div class="content">
-                <img src="imgonglet.png" alt="Photo du groupe" width="50px"/>
+                <img src="imgonglet.png" alt="Photo du groupe" width="450px"/>
             </div>
         </div>
         <div class="col-8-12" align="center">
@@ -44,7 +45,7 @@ $data = json_decode($json);
         </div>
         <div class="col-2-12">
             <div class="content">
-
+                <img src="logo-iteracode.jpg" alt="Iteracode" width="250px"/>
             </div>
         </div>
     </div>
