53e0893be430f9d92a29507f8c085587f5d796e7
antoine.caccin@etud.u-picardie.fr <antoine.caccin@etud.u-picardie.fr>
2018-01-19 02:38:41 +0100

    MAINTENANT !!!

--- Showtimes.php
+++ Showtimes.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$showtimes = array('1' => 'Star Wars', '2' => 'Le seigneur des anus');
+//$showtimes = array('1' => 'Star Wars', '2' => 'Le seigneur des anus');
+$showtimes = array("Id" => array(1,2,3));
 echo (json_encode($showtimes));
 ?>
