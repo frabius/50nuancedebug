ab588c0e80f432a052391d421fe0bf0cf712e2fb
antoine.caccin@etud.u-picardie.fr <antoine.caccin@etud.u-picardie.fr>
2018-01-19 03:01:25 +0100

    maintenant ta race tu fonctionnes

--- Showtimes.php
+++ Showtimes.php
@@ -1,6 +1,16 @@
 <?php
 header('Content-Type: application/json');
-//$showtimes = array('1' => 'Star Wars', '2' => 'Le seigneur des anus');
-$showtimes = array("Id" => array(1,2,3));
+$showtimes = [['id' => 1, 'name' = > 'Star Wars']];
 echo (json_encode($showtimes));
 ?>
+// [ [ 'id' => 1, 'name' => 'String' ] ]
+// json_encode($monTab)
+
+/*
+[
+ {
+   id: 1,
+   name: 'String'
+ }
+]
+*/
